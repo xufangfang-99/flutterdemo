9379f205c33fa6a0d3ae5594e090b2c6f6795e35
fannychu <tigertop001@gmail.com>
2025-09-05 14:33:25 +0400

    1

---
+++ lib/main02.dart
@@ -0,0 +1,45 @@
+// 代码块 importM
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text("你好")),
+        body: const MyApp(),
+      ),
+    ),
+  );
+}
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     // TODO: implement build
+//     return const Center(
+//       child: Text(
+//         "你好Flutter 我是一个自定义组件",
+//         textDirection: TextDirection.ltr,
+//         style: TextStyle(color: Color.fromRGBO(190, 143, 253, 1), fontSize: 30),
+//       ),
+//     );
+//   }
+// }
+
+// 代码块 statelessW
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        "你好Flutter 我是一个自定义组件",
+        textDirection: TextDirection.ltr,
+        style: TextStyle(color: Color.fromRGBO(190, 143, 253, 1), fontSize: 30),
+      ),
+    );
+  }
+}

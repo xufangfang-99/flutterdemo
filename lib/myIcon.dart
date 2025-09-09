e6b3e96aa949680cf03e9131568dd2abe7c56e3b
fannychu <tigertop001@gmail.com>
2025-09-09 10:46:53 +0400

    1

--- lib/myIcon.dart
+++ lib/myIcon.dart
@@ -33,6 +33,60 @@ class MyIcon {
     matchTextDirection: true
   );
 
+  static const IconData stone1=IconData(
+    0xe608, 
+    fontFamily: "myIcon01",
+    matchTextDirection: true
+  );
+
+  static const IconData yuqi01=IconData(
+    0xe609,
+    fontFamily: "myIcon01",
+    matchTextDirection: true
+  );
+
+  static const shiqi2=IconData(
+    0xe60b,
+    fontFamily: "myIcon01",
+    matchTextDirection: true
+  );
+
+  static const chonjicheng=IconData(
+    0xe60c,
+    fontFamily: "myIcon01",
+    matchTextDirection: true
+  );
+
+  static const gengtucheng=IconData(
+    0xe60d,
+    fontFamily: "myIcon01",
+    matchTextDirection: true
+  );
+
+  static const baihuimain=IconData(
+    0xe60e,
+    fontFamily: "myIcon01",
+    matchTextDirection: true
+  );
+
+  static const hangtu=IconData(
+    0xe60f,
+    fontFamily: "myIcon01",
+    matchTextDirection: true
+  );
+
+  static const hongheseshengtu=IconData(
+    0xe610,
+    fontFamily: "myIcon01",
+    matchTextDirection: true
+  );
+
+  static const guge=IconData(
+    0xe611,
+    fontFamily: "myIcon01",
+    matchTextDirection: true
+  );
+
 }
 
 

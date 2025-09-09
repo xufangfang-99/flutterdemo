d34a8ff33b58073d8736cdc6084c8013a522613d
fannychu <tigertop001@gmail.com>
2025-09-09 09:47:08 +0400

    1

---
+++ lib/myIcon.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+
+class MyIcon {
+
+  static const IconData queistion=IconData(
+    0xe623, 
+    fontFamily: "myIcon",
+     matchTextDirection: true
+  );
+
+  static const IconData inp=IconData(
+    0xe624, 
+    fontFamily: "myIcon", 
+    matchTextDirection: true
+  );
+
+  static const IconData straw=IconData(
+    0xe626, 
+    fontFamily: "myIcon",
+     matchTextDirection: true
+  );
+
+  static const IconData catGroup=IconData(
+    0xe627, 
+    fontFamily: "myIcon", 
+    matchTextDirection: true
+  );
+
+  static const IconData notic=IconData(
+    0xe628, 
+    fontFamily: "myIcon", 
+    matchTextDirection: true
+  );
+
+}
+
+
+
+
